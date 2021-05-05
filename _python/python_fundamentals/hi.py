9ef5d0d363eca08e1ad8dc35f43d2020016666e8
Yousef-Nour <yousefnour809@gmail.com>
2021-05-05 15:12:03 +0300

    the third assignment of python

---
+++ _python/python_fundamentals/hi.py
@@ -0,0 +1,18 @@
+# function sum(x,y){
+#     var s = x + y;
+#     return s;
+# }
+
+def sum(x=4, y=5):
+    s = x + y
+    return s
+# print (sum(5, 6))
+
+# a = 3
+# b = 4
+# s = sum(a, b)
+# print(s)
+
+print(sum())   #9
+print(sum(3))   #8
+print(sum(y=3)) #7
