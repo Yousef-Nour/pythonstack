9014590612855148dfc1757a40d7aeb6d20d5fa7
Yousef-Nour <yousefnour809@gmail.com>
2021-05-08 01:39:41 +0300

    dasd
    asd
    sadsad,;lsd

---
+++ _python/python_fundamentals/Functions Basic I/functions_basic_I.py
@@ -0,0 +1,47 @@
+#1
+5
+
+#2
+10
+
+#3
+5
+
+#4
+5
+
+#5
+5
+none
+
+#6
+3 , 5 , Error
+
+#7
+25  #If I used the comma insted the plus sign the result will be ('2', '5')
+
+#8
+I predicted wrong. >> 100,10
+Real Answer Error
+
+#9
+7 , 14 , 21
+
+#10
+8
+
+#11
+500 , 500 , 300 , 500
+
+
+#12
+500 , 500 , 300 , 500
+
+#13
+500 , 500 , 300 , 300
+
+#14
+1 , 3 , 2
+
+#15
+1 , 3 , 5 , 10
