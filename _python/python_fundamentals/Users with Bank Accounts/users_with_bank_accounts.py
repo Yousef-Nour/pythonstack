f90800a3c24865e7014d54c93e0811ab552aae2e
Yousef-Nour <yousefnour809@gmail.com>
2021-05-11 01:45:58 +0300

    PYTHON V4

--- _python/python_fundamentals/Users with Bank Accounts/users_with_bank_accounts.py
+++ _python/python_fundamentals/Users with Bank Accounts/users_with_bank_accounts.py
@@ -1,19 +1,22 @@
-from BankAccount import BankAccount
+from bank_account import BankAccount
 class User:
-    def __init__(self, name, year_of_birth, balance=0):
+    def __init__(self, name, year_of_birth, balance=0, int_rate=0.01):
         self.name = name
         self.year_of_birth = year_of_birth
-        self.account = BankAccount(balance=balance)
+        self.account = BankAccount(balance=balance, int_rate=int_rate)
     
     def make_withdrawal(self, amount):
-        return.self.account.withdraw(amount)
+        self.account.withdraw(amount)
+        return self
     
     def display_user_balance(self):
-        print(f"{self.name} balance is:" end=" ")
+        print(f"{self.name} balance is: ")
         self.account.display_account_info()
+        return self
 
     def deposit(self, amount):
     	self.account.deposit(amount)	# hmmm...the User class doesn't have an account_balance attribute anymore
+        
 
     def transfer_money(self, other_user, amount):
         if self.make_withdrawal(amount):
@@ -21,14 +24,14 @@ class User:
             return True
         return False
     
-    hani= User("hani",1992)
-    alaa= User("alaa",1988)
+hani= User("hani",1992)
+alaa= User("alaa",1988)
 
-    alaa.make_withdrawal(1000)
-    alaa.display_user_balance()
+alaa.make_withdrawal(1000)
+alaa.display_user_balance()
 
-    hani.transfer_money(alaa, 11000)
+hani.transfer_money(alaa, 11000)
 
-    hani.display_user_balance()
-    alaa.display_user_balance()
+hani.display_user_balance()
+alaa.display_user_balance()
 
